e9bc52f8789d89bbd48d1400a975e5029c8828b8
Omar Regalado <regaladomendozaomar@gmail.com>
2023-08-24 17:33:45 -0600

    Initial Commit

---
+++ RickandMortyWiki/RickandMortyWikiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickandMortyWikiApp.swift
+//  RickandMortyWiki
+//
+//  Created by Omar Regalado on 24/08/23.
+//
+
+import SwiftUI
+
+@main
+struct RickandMortyWikiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
