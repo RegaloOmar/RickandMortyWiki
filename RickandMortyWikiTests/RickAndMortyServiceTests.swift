4d9ecc25af758cd0724b96ff87a396d2b4b92c94
Omar Regalado <regaladomendozaomar@gmail.com>
2023-09-01 14:25:17 -0600

    New Tests for Service Class

--- RickandMortyWikiTests/RickAndMortyServiceTests.swift
+++ RickandMortyWikiTests/RickAndMortyServiceTests.swift
@@ -1,58 +1,89 @@
-//
-//  RickAndMortyServiceTests.swift
-//  RickandMortyWikiTests
-//
-//  Created by Omar Regalado on 28/08/23.
-//
-
 import XCTest
+
 @testable import RickandMortyWiki
 
 class RickAndMortyServiceTests: XCTestCase {
- 
-    var rickAndMortyService: RickAndMortyService!
     
+    var rickAndMortyService: RickAndMortyService!
+
     override func setUp() {
         super.setUp()
         rickAndMortyService = RickAndMortyService()
     }
-    
+
     override func tearDown() {
         rickAndMortyService = nil
         super.tearDown()
     }
-    
-    // Test getCharactersInfo() method
-    func testGetCharactersInfo() async throws {
-        let charactersInfo = try await rickAndMortyService.getCharactersInfo()
-        XCTAssertNotNil(charactersInfo)
+
+    func testGetCharactersInfo() {
+        let expectation = XCTestExpectation(description: "Get characters information")
+        
+        Task {
+            do {
+                let charactersInfo = try await rickAndMortyService.getCharactersInfo()
+                XCTAssertNotNil(charactersInfo)
+            } catch {
+                XCTFail("Error: \(error)")
+            }
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
-    
-    // Test loadMore(url:) method
-    func testLoadMore() async throws {
-        let url = "https://rickandmortyapi.com/api/character/?page=2"
-        let nextPage = try await rickAndMortyService.loadMore(url: url)
-        XCTAssertNotNil(nextPage)
+
+    func testLoadMore() {
+        let expectation = XCTestExpectation(description: "Load more characters")
+        
+        Task {
+            do {
+                let url = "https://rickandmortyapi.com/api/character/?page=2"
+                let charactersInfo = try await rickAndMortyService.loadMore(url: url)
+                XCTAssertNotNil(charactersInfo)
+            } catch {
+                XCTFail("Error: \(error)")
+            }
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
-    
-    // Test getCharactersWithFilters(status:) method with alive status
-    func testGetCharactersWithFilters() async throws {
-        let status = "alive"
-        let charactersInfo = try await rickAndMortyService.getCharactersWithFilters(status: status)
-        XCTAssertNotNil(charactersInfo)
+
+    func testGetCharactersWithFilters() {
+        let expectation = XCTestExpectation(description: "Get characters with filters")
+        
+        Task {
+            do {
+                let status = "alive"
+                let charactersInfo = try await rickAndMortyService.getCharactersWithFilters(status: status)
+                XCTAssertNotNil(charactersInfo)
+            } catch {
+                XCTFail("Error: \(error)")
+            }
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
-    
-    // Test fetchEpisodes(from:) method
-    func testFetchEpisodes() async throws {
-        let episodeURLs = [
-            "https://rickandmortyapi.com/api/episode/10",
-            "https://rickandmortyapi.com/api/episode/28",
-            "https://rickandmortyapi.com/api/episode/1",
-            "https://rickandmortyapi.com/api/episode/50"
-        ]
-        let episodes = try await rickAndMortyService.fetchEpisodes(from: episodeURLs)
-        XCTAssertNotNil(episodes)
-        XCTAssertEqual(episodes.count, episodeURLs.count)
+
+    func testFetchEpisodes() {
+        let expectation = XCTestExpectation(description: "Fetch episodes")
+        
+        Task {
+            do {
+                let episodeURLs = ["https://rickandmortyapi.com/api/episode/1"]
+                let episodes = try await rickAndMortyService.fetchEpisodes(from: episodeURLs)
+                XCTAssertFalse(episodes.isEmpty)
+            } catch {
+                XCTFail("Error: \(error)")
+            }
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 10.0)
     }
 }
-
